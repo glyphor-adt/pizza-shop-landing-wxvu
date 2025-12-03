50a15b60f95339063a30ae696c65e43a740fd3d5
glyphor-adt <Kristina@Glyphorai.onmicrosoft.com>
2025-12-03 16:29:07 -0600

    Add src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Hero from './components/Hero'
+import Features from './components/Features'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div className="">
+      <Hero />
+      <Features />
+      {/* <Pricing /> */}
+      <Footer />
+    </div>
+  )
+}
+
+export default App
